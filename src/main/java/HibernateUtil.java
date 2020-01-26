6fe34bddaeb281109fde370bd8d7fdf15672b907
MichalKolban <michal.kolban@gmail.com>
2020-01-26 23:44:59 +0100

    ver 0.04 - clean up, methods still missing

--- src/main/java/HibernateUtil.java
+++ src/main/java/HibernateUtil.java
@@ -19,7 +19,7 @@ public class HibernateUtil {
 				settings.put(Environment.DRIVER,"com.mysql.cj.jdbc.Driver");
 				settings.put(Environment.URL, "jdbc:mysql://localhost:3306/secret_santa?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
                 settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "Santa123@");
+                settings.put(Environment.PASS, "");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 
                 settings.put(Environment.SHOW_SQL, "true");
